7e4a5faa1f30fffbc46e1b744cc504e459271b16
hwangjuhwan <jhanhwang@kicc.co.kr>
2022-11-08 15:37:17 +0900

    init

---
+++ src/main/java/inu/deliverymoa/DeliveryMoaApplication.java
@@ -0,0 +1,13 @@
+package inu.deliverymoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryMoaApplication.class, args);
+    }
+
+}
